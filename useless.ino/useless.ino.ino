0e577f87d9ed4da229cb0885771a8a07ada38fbb
4m1g0 <dev.4m1g0@gmail.com>
2015-04-26 01:01:24 +0200

    Add debug code

--- useless.ino/useless.ino.ino
+++ useless.ino/useless.ino.ino
@@ -7,6 +7,7 @@ const int boton2 = 7;
  
 void setup() 
 { 
+  Serial.begin(9600);
   servo1.attach(10); 
   servo2.attach(11);
   pinMode(boton1, INPUT_PULLUP);
@@ -16,6 +17,7 @@ void setup()
 void loop() 
 { 
   if (digitalRead(boton1) == LOW){
+    Serial.println("boton1");
     delay(100);
     servo1.write(90);
     delay(800);
@@ -23,10 +25,12 @@ void loop()
   }
   
   if (digitalRead(boton2) == LOW){
+    Serial.println("boton2");
     delay(100);
     servo2.write(90);
     delay(800);
     servo2.write(0);
   }
+  delay(10);
 } 
 
